642a5afdbe5dcf27b89d7b2c8544e186f3bbfd9a
ryan0122 <ryan.mack1200@gmail.com>
2017-12-26 23:32:01 -0700

    finalize and clean up code

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -6,7 +6,8 @@ class RatingsController < ApplicationController
   def create
     @rating = Rating.new params.fetch(:rating, {}).permit(:score)
     @rating.save!
-    redirect_to root_url
+    render partial: 'meter'
+    # redirect_to root_url
   rescue ActiveRecord::RecordInvalid
     flash[:error] = 'Could not save score'
     render :index
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def pluralize_word(count, singular, plural = nil)
+    ((count == 1 || count == '1') ? singular : (plural || singular.pluralize))
+  end
+
+  def svg(name)
+    file_path = "#{Rails.root}/app/assets/images/#{name}.svg"
+    return File.read(file_path).html_safe if File.exists?(file_path)
+    '(not found)'
+  end
+
 end
