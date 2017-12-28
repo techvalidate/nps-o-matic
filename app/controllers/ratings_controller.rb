7761000f809e463d1ae3e8b788f0da515e43ace5
cookieroz <cookieroz@gmail.com>
2017-12-28 07:30:47 -0800

    Initial javascript to submit without form refresh

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,15 +1,30 @@
 class RatingsController < ApplicationController
+  skip_before_action :verify_authenticity_token # to avoid 422 error https://stackoverflow.com/questions/27098239/post-422-unprocessable-entity-in-rails-due-to-the-routes-or-the-controller
 
   def index
   end
 
   def create
     @rating = Rating.new params.fetch(:rating, {}).permit(:score)
-    @rating.save!
-    redirect_to root_url
+
+    if @rating.save
+      render json: @rating
+    else
+      render json: @rating.errors, status: :unprocessable_entity
+    end
+
   rescue ActiveRecord::RecordInvalid
     flash[:error] = 'Could not save score'
     render :index
+  end
+
+  def nps_attributes
+    render json: {
+      total_ratings: Rating.count,
+      nps: Rating.nps,
+      detractors_percentage: Rating.detractors_percentage,
+      promoters_percentage: Rating.promoters_percentage
+    }
   end
 
 end
