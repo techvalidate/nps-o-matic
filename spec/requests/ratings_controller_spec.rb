f97a10f1ec4e0c9f1b55545a60cbf2e41c0c8a5a
cookieroz <cookieroz@gmail.com>
2017-12-30 04:38:03 -0800

    Pass specs

--- spec/requests/ratings_controller_spec.rb
+++ spec/requests/ratings_controller_spec.rb
@@ -10,21 +10,21 @@ describe RatingsController do
   it 'saves a rating' do
     expect {
       post ratings_url, params: { rating: { score: 10 } }
-      expect(response).to redirect_to(root_url)
+      expect(response).to be_success
     }.to change(Rating, :count).by(1)
   end
 
   it 'handles an empty ratings hash' do
     expect {
       post ratings_url
-      expect(response).to be_success
+      expect(response).to have_http_status(422)
     }.not_to change(Rating, :count)
   end
 
   it 'handles an invalid rating score' do
     expect {
       post ratings_url, params: { rating: { score: 11 } }
-      expect(response).to be_success
+      expect(response).to have_http_status(422)
     }.not_to change(Rating, :count)
   end
 
