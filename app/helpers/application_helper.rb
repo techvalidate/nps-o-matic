defeb2bab1bb5049ccbabdb82d483582aca98268
Ami Suzuki <ami.suzu@gmail.com>
2018-07-21 13:37:53 -0700

    Gauge with rotation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+	# Taken from https://stackoverflow.com/questions/36986925/how-do-i-display-svg-image-in-rails
+	def show_svg(path)
+	  File.open("app/assets/images/#{path}", "rb") do |file|
+	    raw file.read
+	  end
+	end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,3 +8,15 @@
 // layout file, like app/views/layouts/application.html.erb
 
 console.log('Hello World from Webpacker');
+
+window.onload = function() {
+	// TODO: Update color gauge color
+	// document.getElementById("Oval-3").style.fill = "yellow";
+	// document.getElementById("caret-copy-2").style.fill = "yellow";
+
+	// Rotate gauge
+	var npsValue = document.getElementById("npsValue").getAttribute('data-nps');
+	document.getElementById("gaugeRotater").setAttribute("transform", "rotate(" + npsValue + ", 86.5, 86)");
+}
+
+
