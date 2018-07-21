5944ccc7b8caa479e656eb80b04a5a56d0f87813
Ami Suzuki <ami.suzu@gmail.com>
2018-07-21 14:05:28 -0700

    NPS dial color change; update NPS dial text

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,16 +7,42 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+/* color reference for gauge */
+r_neg_100 = 210;
+g_neg_100 = 41;
+b_neg_100 = 83;
+
+r_0 = 249;
+g_0 = 190;
+b_0 = 0;
+
+r_100 = 35;
+g_100 = 211;
+b_100 = 133;
+
 console.log('Hello World from Webpacker');
 
 window.onload = function() {
-	// TODO: Update color gauge color
-	// document.getElementById("Oval-3").style.fill = "yellow";
-	// document.getElementById("caret-copy-2").style.fill = "yellow";
+	var npsValue = parseInt(document.getElementById("npsValue").getAttribute('data-nps'));
+	if (npsValue < 0) {
+		var r = Math.round(r_0 + (r_0 - r_neg_100) * (npsValue / 100));
+		var g = Math.round(g_0 + (g_0 - g_neg_100) * (npsValue / 100));
+		var b = Math.round(b_0 + (b_0 - b_neg_100) * (npsValue / 100));
+	} else {
+		var r = Math.round(r_0 + (r_100 - r_0) * (npsValue / 100));
+		var g = Math.round(g_0 + (g_100 - g_0) * (npsValue / 100));
+		var b = Math.round(b_0 + (b_100 - b_0) * (npsValue / 100));
+	}
+	// Update color gauge color
+	var rgbString = "rgb(" + r + "," + g + "," + b + ")";
+	document.getElementById("Oval-3").style.fill = rgbString
+	document.getElementById("caret-copy-2").style.fill = rgbString;
 
 	// Rotate gauge
-	var npsValue = document.getElementById("npsValue").getAttribute('data-nps');
 	document.getElementById("gaugeRotater").setAttribute("transform", "rotate(" + npsValue + ", 86.5, 86)");
+
+	// Update gauge text
+	document.getElementById("npsValue").textContent = npsValue;
 }
 
 
