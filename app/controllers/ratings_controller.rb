4cdf07ea4533c9b99edea4ef0262f603d931a3cc
Ami Suzuki <ami.suzu@gmail.com>
2018-07-21 23:58:42 -0700

    Don't require page reload for appreciate section

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,4 +1,6 @@
 class RatingsController < ApplicationController
+  respond_to :html, :js, :json
+  protect_from_forgery except: :create
 
   def index
   end
@@ -6,7 +8,16 @@ class RatingsController < ApplicationController
   def create
     @rating = Rating.new params.fetch(:rating, {}).permit(:score)
     @rating.save!
-    redirect_to root_url
+    respond_to do |format|
+      # format.html {redirect_to root_url }
+      # format.json { render json: @rating }
+      # format.js {render :content_type => 'text/javascript'}
+      format.js { render layout: false, content_type: 'text/javascript' }
+        # format.js
+      # redirect_to root_url
+    end
+    #     @rating = Rating.new params.fetch(:rating, {}).permit(:score)
+    # @rating.save!
   rescue ActiveRecord::RecordInvalid
     flash[:error] = 'Could not save score'
     render :index
