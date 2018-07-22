73a940fca85b8fc3e3cdbb578a68e9d11a03c9fd
Ami Suzuki <ami.suzu@gmail.com>
2018-07-22 00:15:20 -0700

    removing commented out code

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -9,15 +9,8 @@ class RatingsController < ApplicationController
     @rating = Rating.new params.fetch(:rating, {}).permit(:score)
     @rating.save!
     respond_to do |format|
-      # format.html {redirect_to root_url }
-      # format.json { render json: @rating }
-      # format.js {render :content_type => 'text/javascript'}
       format.js { render layout: false, content_type: 'text/javascript' }
-        # format.js
-      # redirect_to root_url
     end
-    #     @rating = Rating.new params.fetch(:rating, {}).permit(:score)
-    # @rating.save!
   rescue ActiveRecord::RecordInvalid
     flash[:error] = 'Could not save score'
     render :index
