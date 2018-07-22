d3ff7aeb581158fad50fea0a02b825aeb526e948
Ami Suzuki <ami.suzu@gmail.com>
2018-07-21 23:54:23 -0700

    adding required modules; jQuery, jquery-rails

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
-const { environment } = require('@rails/webpacker');
 
-module.exports = environment;
+const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+module.exports = environment
+
+environment.plugins.set(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery'
+  })
+)
